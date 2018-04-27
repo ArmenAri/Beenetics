ffe1ad1b420e8f0b33733c6b62fddefa634d31d0
harry <harry@192.168.1.90>
2018-04-27 21:58:41 +0200

    Server is now an object.

--- ServerBeenetics/src/fr/armenari/beeneticsserver/main/Server.java
+++ ServerBeenetics/src/fr/armenari/beeneticsserver/main/Server.java
@@ -4,17 +4,35 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 public class Server {
+	private ServerSocket serverSocket;
+	private Thread clientListenerThread;
 	
-	public static void main(String[] args) {
-		ServerSocket serverSocket;
+	public Server(int port) {
 		try {
-			serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+			serverSocket = new ServerSocket(port);
 			Request.peer();
-			Thread t1 = new Thread(new AcceptClient(serverSocket));
-			t1.start();
+			clientListenerThread = new Thread(new AcceptClient(serverSocket), "Client Listener - Beenetics Server");
+			clientListenerThread.start();
 			System.out.println("[SERVER] : Thread started !");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}	
+	}
+	
+	public void stop() {
+		
+	}
+	
+	public static void main(String[] args) {
+		Server s = null;
+		try {
+			s = new Server(Integer.parseInt(args[0]));
+		} catch (NumberFormatException e) {
+			System.err.println("You must enter a valid port !");
+			e.printStackTrace();
+		} finally {
+			if (null != s) s.stop();
+			System.out.println("Server stopped.");
+		}
 	}
 }
